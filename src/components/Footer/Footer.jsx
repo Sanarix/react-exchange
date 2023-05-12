8964c95208952925408b6e4af08206293f17390c
Sanarix <KorsakovLA@mail.ru>
2023-05-12 13:43:03 +0300

    обновил стили для мобильных устройств, добавил изображение погоды

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 
 	return (
 		<footer className={styles.footer}>
-			<div>
+			<div className={styles.owner}>
 				Disign and develop by <a href="https://korsakovla.web.app/">Leonid Korsakov</a>
 			</div>
 			<div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,74 +1,104 @@
 import styles from './style.module.css';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import weatherConfig from '../../config/weatherApi.json';
 import cities from '../../cities/cities.json';
 
 
 const Main = () => {
-	const [resData, setResData] = useState(null);
-	const [cityCoords, setCityCoords] = useState(null);
+	const [cityName, setCityName] = useState('');
 	const [weatherInfo, setWeatherInfo] = useState(null);
+	const [searchQuery, setSearchQuery] = useState('');
+	const [seacrhSortedList, setSearchSortedList] = useState(null);
 
-	useEffect(() => {
-		setCityCoords({lat: cities[0].lat, lon: cities[0].lon})
-	}, [])
-
-	useEffect(() => {
-		console.log(cityCoords);
-	}, [cityCoords])
-
-	useEffect(() => {
-		console.log(weatherInfo);
-	}, [weatherInfo])
-
-	function selectHandler(e) {
-		const city = cities.filter((el) => 
-			el.name === e.target.value
-		)[0]
-		setCityCoords({lat: city.coords.lat, lon: city.coords.lon})
-	}
-
-		function getWeather() {
-			fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityCoords.lat}&lon=${cityCoords.lon}&appid=${weatherConfig['API-Key']}`)
+	function handler(e) {
+		const city = seacrhSortedList.filter((el) => 
+			el.name === e.target.textContent
+		)[0];
+		setCityName(city.name)
+		fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lon}&appid=${weatherConfig['API-Key']}&lang=ru`)
 			.then(res => res.json())
 			.then(res => {
+				console.log(res);
 				setWeatherInfo(res);
 			})
 			.catch(err => {
 				throw new Error(err.message)
 			})
+			setSearchQuery('');
+			setSearchSortedList(null);
+	}
+
+		function getWindDir(deg) {
+			switch(true) {
+				case deg === 0 || deg === 360: return 'С';
+				case deg > 0 && deg < 90: return 'СВ'
+				case deg === 90: return 'В';
+				case deg > 90 && deg < 180: return 'ЮВ'
+				case deg === 180: return 'Ю';
+				case deg > 180 && deg < 270: return 'ЮЗ'
+				case deg === 270: return 'З';
+				case deg > 270 && deg < 360: return 'СЗ'
+				default: return 'Not wind'
+			}
 		}
+
+		function searchHandler(value) {
+			if(value.length) {
+				setSearchSortedList(cities.filter(el => el.name.toLowerCase().slice(0, value.length).includes(value.toLowerCase())));
+			}else {
+				setSearchSortedList(null)
+			}
+		}
+
+		useEffect(() => {
+			searchHandler(searchQuery)
+		}, [searchQuery])
 
 	return (
 		<main className={styles.main}>
-			<select onChange={selectHandler}>
-				{cities.map((el, i) => {
-					return (
-						<option key={i} value={el.name}>
-							{el.name}
-						</option>
-					)
-				})}
-			</select>
-			<button onClick={getWeather}>Сделать запрос</button>
-			{/* {weatherInfo && 
-						<div className={styles.weatherCard}>
-						<h1 className={styles.weatherCardHeader}>{data.location.name}</h1>
-						<div className={styles.weatherCardBody}>
-							<ul>
-								<li>Current weather: {data.current.weather_descriptions[0]}</li>
-								<li>Time: {data.current.observation_time}</li>
-								<li>Temperature: {data.current.temperature}</li>
-								<li>Pressure: {data.current.pressure}</li>
-								<li>Wind speed: {data.current.wind_speed}</li>
-								<li>Wind direction: {data.current.wind_dir}</li>
-								<li>{'Visibility (m)'} : {data.current.visibility}</li>
-								<li>UV-index: {data.current.uv_index}</li>
-								<li><img src={data.current.weather_icons} alt="weather-icon"/></li>
-							</ul>
-						</div>
-					</div>
-			} */}
+			<div className={styles.searchWrapper}>
+				<input className={styles.searchInput} onChange={(e) => {setSearchQuery(e.target.value)}} value={searchQuery} placeholder='Поиск города'></input>
+				<ul id="cities-list"className={styles.citiesList}>
+					{seacrhSortedList && 
+					seacrhSortedList.map((el, i) => {
+						return (
+							<li key={i} onClick={handler}>
+								{el.name}
+							</li>
+						)
+					})
+					}
+					{
+						searchQuery &&
+						seacrhSortedList?.length === 0 &&
+						<li>Совпадений не найдено</li>
+					}
+				</ul>
+			</div>
+			{weatherInfo && 
+				<div className={styles.weatherCard}>
+					<h1 className={styles.weatherCard_header}>{cityName}</h1>
+					<h2 className={styles.weatherCard_subheader}> Сейчас {weatherInfo.weather[0].description}
+					</h2>
+					<img className={styles.weatherCard_image} src={` https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png`} alt="weather-icon" />
+					<ul className={styles.weatherCard_list}>
+						<li>Температура: {
+						Math.floor(weatherInfo.main.temp - 273.15)
+						}{'\u00b0'}C
+						</li>
+						<li>Ощущается как: {
+						Math.floor(weatherInfo.main.feels_like - 273.15)
+						}{'\u00b0'}C
+						</li>
+						<li>Влажность: {weatherInfo.main.humidity} %</li>
+						<li>Давление: {(weatherInfo.main.pressure * 0.75).toFixed()} мм.рт.ст.</li>
+						<li>Ветер: {Math.floor(weatherInfo.wind.speed)} м/с</li>
+						<li>Порывы ветра до: {Math.floor(weatherInfo.wind.gust) || 0}  м/с</li>
+						<li>Направление ветра: {getWindDir(weatherInfo.wind.deg)}</li>
+						<li>Процент облачности: {weatherInfo.clouds.all} %</li>
+					</ul>
+				</div>
+			}
 		</main>
 	)
 }
